36b2c2178757b7f948702cb7fac169448e77575c
XenonLemon <pd922503@gmail.com>
2026-02-10 16:16:56 +0000

    Commit 2 2/10/26

--- CanAttend.java
+++ CanAttend.java
@@ -1,14 +1,28 @@
-
+//Peter Daniel 2/10/26
 import java.util.ArrayList;
 
 public class CanAttend {
 
-	//precondition: 
-	//postcondition: 
+	//precondition: The ArrayList containing the Meeting intervals is not null and does not contain any null values.  
+	//postcondition: Will output if false if any meetings start and end times overlap with each other, and true if the meetings don't conflict each other.
 	public static boolean canAttend(ArrayList<MeetingInterval> meetings) {
 		//your implementation here
 		System.out.println(meetings);
-		return false; //replace me with actual logic please!
+		
+		for(int i = 0; i < meetings.size(); i++){
+			for(int j = i+1; j<meetings.size(); j++){
+				
+				int start1 = meetings.get(i).getStart();
+				int end1 = meetings.get(i).getEnd();
+				int start2 = meetings.get(j).getStart();
+				int end2 = meetings.get(j).getEnd();
+				if(!(start1 < start2 && end1 < end2 && start1 < end2 && start2 > end1 && end1 > start1 && end2 > start2)){
+					return false;
+				}
+			}
+		}
+		return true;
+
 	}
 	public static void main(String[] args) {
 		ArrayList<MeetingInterval> meet = new ArrayList<MeetingInterval>();
@@ -16,8 +30,16 @@ public class CanAttend {
 			int start = (int)(Math.random()*2399);
 			int end = start + ((int)(Math.random()*(2399-start)));
 			meet.add(new MeetingInterval(start, end));
+
 		}
 		System.out.println(canAttend(meet) ? "There is no conflict with attending "+meet+" meetings":"Can't attend "+meet+" meetings due to conflict");
 		//please include additional testing here!
+		meet.clear();
+		meet.add(new MeetingInterval(0,20));
+		meet.add(new MeetingInterval(60 ,100));
+		//meet.add(new MeetingInterval(90 ,120));
+		//meet.add(new MeetingInterval(160 ,140));
+		System.out.println(canAttend(meet) ? "There is no conflict with attending "+meet+" meetings":"Can't attend "+meet+" meetings due to conflict");
+
 	}
 }
